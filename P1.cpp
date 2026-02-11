1e5eed972e2e4ae746ce4d8845f3898ca2f8d88f
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2026-02-11 12:21:42 -0500

    Initial Commit

--- P1.cpp
+++ P1.cpp
@@ -5,15 +5,9 @@ using namespace std;
 int main() {
     double volume,d,h;
     // Place your solution code for calculating the area of the triangle using the Heron's formula here
-    cout << "Enter value of width (diameter) of the cylinder: ";
-    cin >> d;
-    cout << "Enter value of height of the cylinder: ";
-    cin >> h;
-
-
-    volume = M_PI * pow((d / 2),2) * h;
     
-    // Use this code to output the area of the triangle
+    
+    // Use this code to output the volume of the cylinder
     cout.setf(ios::fixed);
     cout.precision(2);
     cout << "\nThe volume of the cylinder is " << volume << endl;
